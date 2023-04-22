af26aa64546cabaac7bd3bf34182e4b44f4cc618
palmer <palmerb4@wit.edu>
2023-04-22 17:42:39 -0400

    Added support for unit outward normals. Needs tested

--- src/ccqppy/solution_spaces.py
+++ src/ccqppy/solution_spaces.py
@@ -26,9 +26,23 @@ class ProjOpBase(ABC):
         """
         pass
 
+    @abstractmethod
+    def normal_vector(self, x):
+        """Return the unit outward normal of the projection operator at the point x on the boundary.
+
+        If x is inside the boundary, returns the zero vector.
+        If x is outside the boundary, projects x onto the boundary and then returns the normal there.
+        """
+        pass
+
     @abstractproperty
     def name(self):
         """Return the name of the projection operator."""
+        pass
+
+    @abstractproperty
+    def embedded_dimension(self):
+        """Return the embedded dimension of the projection operator."""
         pass
 
     def plot(self, embedded_dimension, num_random_samples, lower_bound, upper_bound):
@@ -59,6 +73,7 @@ class ProjOpBase(ABC):
 
 class IdentityProjOp(ProjOpBase):
     def __init__(self, embedded_dimension):
+        self.dim = embedded_dimension
         pass
 
     @property
@@ -66,8 +81,21 @@ class IdentityProjOp(ProjOpBase):
         """Return the name of the projection operator."""
         return "Identity"
 
+    @property
+    def embedded_dimension(self):
+        """Return the embedded dimension of the projection operator."""
+        return self.dim
+
+    def normal_vector(self, x):
+        """Return the unit outward normal of the projection operator at the point x on the boundary.
+
+        If x is inside the boundary, returns the zero vector.
+        If x is outside the boundary, projects x onto the boundary and then returns the normal there.
+        """
+        return np.zeros(self.dim)
+
     def __call__(self, x):
-        """Projection operation for the space 
+        """Projection operation for the space
         {x in R^n}
 
         Parameters
@@ -85,6 +113,7 @@ class IdentityProjOp(ProjOpBase):
 
 class LowerBoundProjOp(ProjOpBase):
     def __init__(self, embedded_dimension, lower_bound=None):
+        self.dim = embedded_dimension
         if lower_bound is not None:
             self.lower_bound = lower_bound
         else:
@@ -95,8 +124,29 @@ class LowerBoundProjOp(ProjOpBase):
         """Return the name of the projection operator."""
         return "Lower Bound"
 
+    @property
+    def embedded_dimension(self):
+        """Return the embedded dimension of the projection operator."""
+        return self.dim
+
+    def normal_vector(self, x):
+        """Return the unit outward normal of the projection operator at the point x on the boundary.
+
+        If x is inside the boundary, returns the zero vector.
+        If x is outside the boundary, projects x onto the boundary and then returns the normal there.
+        """
+        x_proj = self.__call__(x)
+        if (not np.isclose(np.linalg.norm(x - x_proj), 0)):
+            return np.zeros(self.dim)
+        else:
+            norm = np.zeros(self.dim)
+            for i in range(self.dim):
+                if np.isclose(x_proj[i], self.lower_bound[i]):
+                    norm[i] = -1
+            return norm
+
     def __call__(self, x):
-        """Projection operation for the space 
+        """Projection operation for the space
         {x in R^n : x >= lb} for some lower bound lb in R^n
 
         Parameters
@@ -115,6 +165,7 @@ class LowerBoundProjOp(ProjOpBase):
 
 class UpperBoundProjOp(ProjOpBase):
     def __init__(self, embedded_dimension, upper_bound=None):
+        self.dim = embedded_dimension
         if upper_bound is not None:
             self.upper_bound = upper_bound
         else:
@@ -125,8 +176,29 @@ class UpperBoundProjOp(ProjOpBase):
         """Return the name of the projection operator."""
         return "Upper Bound"
 
+    @property
+    def embedded_dimension(self):
+        """Return the embedded dimension of the projection operator."""
+        return self.dim
+
+    def normal_vector(self, x):
+        """Return the unit outward normal of the projection operator at the point x on the boundary.
+
+        If x is inside the boundary, returns the zero vector.
+        If x is outside the boundary, projects x onto the boundary and then returns the normal there.
+        """
+        x_proj = self.__call__(x)
+        if (not np.isclose(np.linalg.norm(x - x_proj), 0)):
+            return np.zeros(self.dim)
+        else:
+            norm = np.zeros(self.dim)
+            for i in range(self.dim):
+                if np.isclose(x_proj[i], self.upper_bound[i]):
+                    norm[i] = 1
+            return norm
+
     def __call__(self, x):
-        """Projection operation for the space 
+        """Projection operation for the space
         {x in R^n : x <= ub} for some upper bound lb in R^n
 
         Parameters
@@ -145,6 +217,7 @@ class UpperBoundProjOp(ProjOpBase):
 
 class BoxProjOp(ProjOpBase):
     def __init__(self, embedded_dimension, lower_bound=None, upper_bound=None):
+        self.dim = embedded_dimension
         if lower_bound is not None:
             self.lower_bound = lower_bound
         else:
@@ -163,8 +236,31 @@ class BoxProjOp(ProjOpBase):
         """Return the name of the projection operator."""
         return "Box"
 
+    @property
+    def embedded_dimension(self):
+        """Return the embedded dimension of the projection operator."""
+        return self.dim
+
+    def normal_vector(self, x):
+        """Return the unit outward normal of the projection operator at the point x on the boundary.
+
+        If x is inside the boundary, returns the zero vector.
+        If x is outside the boundary, projects x onto the boundary and then returns the normal there.
+        """
+        x_proj = self.__call__(x)
+        if (not np.isclose(np.linalg.norm(x - x_proj), 0)):
+            return np.zeros(self.dim)
+        else:
+            norm = np.zeros(self.dim)
+            for i in range(self.dim):
+                if np.isclose(x_proj[i], self.upper_bound[i]):
+                    norm[i] = 1
+                elif np.isclose(x_proj[i], self.lower_bound[i]):
+                    norm[i] = -1
+            return norm
+
     def __call__(self, x):
-        """Projection operation for the space 
+        """Projection operation for the space
         {x in R^n : lb <= x <= ub} for some lower and upper bounds lb/ub in R^n
 
         Parameters
@@ -185,6 +281,7 @@ class BoxProjOp(ProjOpBase):
 
 class SphereProjOp(ProjOpBase):
     def __init__(self, embedded_dimension, radius=None):
+        self.dim = embedded_dimension
         if radius is not None:
             self.radius = radius
         else:
@@ -196,6 +293,27 @@ class SphereProjOp(ProjOpBase):
     def name(self):
         """Return the name of the projection operator."""
         return "Sphere"
+
+    @property
+    def embedded_dimension(self):
+        """Return the embedded dimension of the projection operator."""
+        return self.dim
+
+    def normal_vector(self, x):
+        """Return the unit outward normal of the projection operator at the point x on the boundary.
+
+        If x is inside the boundary, returns the zero vector.
+        If x is outside the boundary, projects x onto the boundary and then returns the normal there.
+        """
+        x_proj = self.__call__(x)
+        if (not np.isclose(np.linalg.norm(x - x_proj), 0)):
+            return np.zeros(self.dim)
+        else:
+            norm = np.zeros(self.dim)
+            x_proj_length = np.linalg.norm(x_proj)
+            if np.isclose(x_proj_length, self.radius):
+                norm = x_proj / x_proj_length
+            return norm
 
     def __call__(self, x):
         """Projection operation for the space
@@ -221,6 +339,7 @@ class SphereProjOp(ProjOpBase):
 class ConeProjOp(ProjOpBase):
     # TODO(palmerb4): This projection op is bugged
     def __init__(self, embedded_dimension, aspect_ratio=None):
+        self.dim = embedded_dimension
         if aspect_ratio is not None:
             self.aspect_ratio = aspect_ratio
         else:
@@ -233,8 +352,78 @@ class ConeProjOp(ProjOpBase):
         """Return the name of the projection operator."""
         return "Cone"
 
+    @property
+    def embedded_dimension(self):
+        """Return the embedded dimension of the projection operator."""
+        return self.dim
+
+    def normal_vector(self, x):
+        """Return the unit outward normal of the projection operator at the point x on the boundary.
+
+        If x is inside the boundary, returns the zero vector.
+        If x is outside the boundary, projects x onto the boundary and then returns the normal there.
+        """
+        raise NotImplementedError("Cone normal not implemented, yet.")
+
     def __call__(self, x):
-        """Projection operation for the space 
+        """Projection operation for the space
+        {[x,z]^T in R^{n-1} x R : x^Tx <= mu^2 z^2} for some hyper-cone aspect ratio mu in R^+
+
+        Parameters
+        ----------
+            x : {array-like, matrix} of shape (n_unknowns, 1)
+                value to project onto the feasible set
+
+        Returns
+        -------
+            x_proj : {array-like, matrix} of shape (n_unknowns, 1)
+                projected value of x
+        """
+        x_norm = np.linalg.norm(x)
+
+        if self.aspect_ratio * x[-1] >= x_norm:
+            return x
+        elif -x[-1] / self.aspect_ratio >= x_norm:
+            return np.zeros_like(x)
+        else:
+            return (x[-1] + self.aspect_ratio * x_norm) / (self.aspect_ratio**2 + 1) \
+                * np.concatenate((x[:-1] / x_norm, [-self.aspect_ratio]))
+
+
+class DisjointProjOp(ProjOpBase):
+    def __init__(self, *convex_proj_ops):
+        self.proj_ops = convex_proj_ops
+        self.dim = 0
+        for proj_op in self.proj_ops:
+            self.dim += proj_op.embedded_dimension
+
+    @property
+    def name(self):
+        """Return the name of the projection operator."""
+        return "DisjointUnion"
+
+    @property
+    def embedded_dimension(self):
+        """Return the embedded dimension of the projection operator."""
+        return self.dim
+
+    def normal_vector(self, x):
+        """Return the unit outward normal of the projection operator at the point x on the boundary.
+
+        If x is inside the boundary, returns the zero vector.
+        If x is outside the boundary, projects x onto the boundary and then returns the normal there.
+        """
+        normal = np.zeros(self.dim)
+        start_index = 0
+        for proj_op in self.proj_ops:
+            dim = proj_op.embedded_dimension
+            normal[start_index: start_index +
+                   dim] = proj_op.normal_vector(x[start_index: start_index + dim])
+            start_index += dim
+        return normal
+
+    def __call__(self, x):
+        """Projection operation for the space
         {[x,z]^T in R^{n-1} x R : x^Tx <= mu^2 z^2} for some hyper-cone aspect ratio mu in R^+
 
         Parameters
